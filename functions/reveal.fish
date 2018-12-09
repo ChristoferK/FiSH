833c5f265dc66ac18e2461ef908d498885667ec4
CK <34463321+ChristoferK@users.noreply.github.com>
2018-12-09 19:23:52 +0000

    added description

--- functions/reveal.fish
+++ functions/reveal.fish
@@ -1,4 +1,4 @@
 # Defined in /var/folders/ht/0ty_c8mx16v69csh949g3rvr0000gn/T//fish.AxqDeD/reveal.fish @ line 2
-function reveal
+function reveal --description 'Reveal a file in Finder'
 	OPEN -R (eval "ECHO $argv")
 end
