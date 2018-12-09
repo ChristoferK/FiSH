5b753e79a9db37f4af4769d6aa31f612d0a9da89
CK <34463321+ChristoferK@users.noreply.github.com>
2018-12-09 17:32:29 +0000

    added description

--- functions/tell.fish
+++ functions/tell.fish
@@ -1,18 +1,20 @@
 # Defined in /var/folders/ht/0ty_c8mx16v69csh949g3rvr0000gn/T//fish.89MA6j/tell.fish @ line 2
-function tell
+function tell --description 'Execute a one-line AppleScript'
 	# No arguments
-	[ -z "$argv" ]; and return (false)
+	[ -z "$argv" ]; \
+		and ECHO "Usage: tell [application|process] %app% to..."; \
+		and return (false)
 
 	[ "$argv[1]" = "process" ]; and \
 		set -l scpt (string replace -r \
 			'process "?(.+?)"? to (.*)' \
-			'tell app "System Events" to tell process "${1}" to ${2}' \
+			'app "System Events" to tell process "${1}" to ${2}' \
 			"$argv"); or \
 		set -l scpt (string replace -r \
 			'(app(lication)? )?"?(.+?)"? to (.*)' \
-			'tell app "${3}" to ${4}' \
+			'app "${3}" to ${4}' \
 			"$argv")
 
-	OSASCRIPT -e "$scpt"
+	OSASCRIPT -e "tell $scpt"
 	return $status
 end
