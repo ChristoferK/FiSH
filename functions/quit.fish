2e8934b6c93a7bcca8c64c1848b97a2608ec2d7e
CK <34463321+ChristoferK@users.noreply.github.com>
2018-12-09 19:24:00 +0000

    update

--- functions/quit.fish
+++ functions/quit.fish
@@ -1,4 +1,8 @@
 # Defined in /var/folders/ht/0ty_c8mx16v69csh949g3rvr0000gn/T//fish.fMU9IS/quit.fish @ line 2
-function quit
-	[ -n "$argv" ]; and OSASCRIPT -e "QUIT the application named \"$argv\""
+function quit --description 'Quit a list of applications'
+	[ -n "$argv[1]" ]; and OSASCRIPT -e \
+		"ignoring application responses
+			QUIT the application named \"$argv[1]\"
+		 end ignoring";
+	[ -n "$argv[2]" ]; and quit $argv[2..-1]
 end
