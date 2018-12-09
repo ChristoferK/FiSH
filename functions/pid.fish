a90e4d043df69748964841e3add9195070eb3296
CK <34463321+ChristoferK@users.noreply.github.com>
2018-12-09 18:19:57 +0000

    minor code correction

--- functions/pid.fish
+++ functions/pid.fish
@@ -1,6 +1,7 @@
 # Defined in /var/folders/ht/0ty_c8mx16v69csh949g3rvr0000gn/T//fish.O9rmJe/pid.fish @ line 2
 function pid
-	[ -z "$argv" ]; and return (false); or \
-			PGREP -i "$argv";
+	[ -z "$argv" ]; \
+		and return (false); \
+		or  PGREP -i $argv;
 	return $status
 end
