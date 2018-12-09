f1b43150330cb47307cf4df739f8fc824d3f7e72
CK <34463321+ChristoferK@users.noreply.github.com>
2018-12-09 19:29:35 +0000

    added description

--- functions/rm.fish
+++ functions/rm.fish
@@ -1,4 +1,4 @@
 # Defined in /var/folders/ht/0ty_c8mx16v69csh949g3rvr0000gn/T//fish.ltamEj/rm.fish @ line 2
-function rm
+function rm --description "Redefines the rm command so that files are trashed instead of deleted"
 	trash $argv
 end
--- functions/trash.fish
+++ functions/trash.fish
@@ -1,6 +1,5 @@
 # Defined in /var/folders/ht/0ty_c8mx16v69csh949g3rvr0000gn/T//fish.C8Fcxx/trash.fish @ line 2
-function trash
-	# mv -v $argv ~/.Trash
+function trash --description "Send files to the trash.  Uses osascript so the deletion can be undone in Finder."
 	set -l fs ( PRINTF "$PWD/%s:" ( BASENAME -a $argv ) )
 
 	PRINTF "SET the TEXT ITEM DELIMITERS to \":\"
