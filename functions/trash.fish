1066b6f1880e69426b9cee4e67056eaae14394f2
CK <34463321+ChristoferK@users.noreply.github.com>
2019-04-27 08:35:36 +0100

    fixed

--- functions/trash.fish
+++ functions/trash.fish
@@ -1,14 +1,20 @@
 # Defined in /var/folders/ht/0ty_c8mx16v69csh949g3rvr0000gn/T//fish.C8Fcxx/trash.fish @ line 2
 function trash --description "Send files to the trash.  Uses osascript so the deletion can be undone in Finder."
-	set -l fs ( PRINTF "$PWD/%s:" ( BASENAME -a $argv ) )
+	for file in $argv
+		set -l dir ( dirname "$file" )
+		[ "$dir" = "." ]; and \
+			set -l fp ( printf "$PWD/%s" \
+			          ( basename -a "$file" ) )
+		set files $files "$file"
+	end
 
-	PRINTF "SET the TEXT ITEM DELIMITERS to \":\"
-		SET fs to TEXT ITEMS 1 thru -2 of \"$fs\"
+	printf "on run fs
+			REPEAT WITH f in fs
+				SET f's contents to ¬
+				    f as POSIX file ¬
+				      as alias
+			END REPEAT
 
-		REPEAT WITH f in fs
-			SET the contents of f to POSIX file f
-		END REPEAT
-
-		TELL app \"Finder\" to DELETE fs" \
-	| OSASCRIPT > /dev/null
+			tell app \"Finder\" to DELETE fs
+		end run" | OSASCRIPT - $files > /dev/null
 end
