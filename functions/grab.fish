009462f0e96662df385ccb62f344424b155d0b04
CK <34463321+ChristoferK@users.noreply.github.com>
2019-05-01 13:54:25 +0100

    changed timestring format to "XXhXXm"

--- functions/grab.fish
+++ functions/grab.fish
@@ -4,7 +4,7 @@ function grab --description 'Take a time-delayed window capture'
                 or  set -l delay $argv[1]
 
         set -l dir (defaults read com.apple.screencapture location)
-        set -l filename "Screen Shot "(date +'%Y-%m-%d at %H.%M.%S')".jpg"
+        set -l filename "Screen Shot "(date +'%Y-%m-%d at %Hh%Mm%S')".jpg"
         set -l path "$dir/$filename"
 
         screencapture -iWoax -t jpg -T $delay "$path"
