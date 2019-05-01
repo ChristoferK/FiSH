64e3d6571264f9f2108094c2007cf379e7baf0b5
CK <34463321+ChristoferK@users.noreply.github.com>
2019-05-01 13:55:42 +0100

    updated syntax

--- functions/reveal.fish
+++ functions/reveal.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/ht/0ty_c8mx16v69csh949g3rvr0000gn/T//fish.AxqDeD/reveal.fish @ line 2
-function reveal --description 'Reveal a file in Finder'
-	OPEN -R (eval "ECHO $argv")
+# Defined in - @ line 1
+function reveal --description 'Reveal files in Finder'
+	open -R (eval "echo $argv")
 end
