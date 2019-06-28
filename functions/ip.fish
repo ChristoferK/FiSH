f1a787912a661ccb6f20fb167f6b765855859a97
CK <34463321+ChristoferK@users.noreply.github.com>
2019-06-28 17:24:41 +0100

    rewrite using ipify.org

--- functions/ip.fish
+++ functions/ip.fish
@@ -1,26 +1,5 @@
-# Defined in /var/folders/ht/0ty_c8mx16v69csh949g3rvr0000gn/T//fish.k9I7RN/ip.fish @ line 2
-function ip
-	set -l curlIP "CURL ipinfo.io --silent \
-		| GREP -E -io '([A-Z0-9,\.-]{2,} ?)+' \
-		| PASTE -sd :\n -"
-	set -l IPonly "| HEAD -1 | GREP -E -io '[^A-Z:]+'"
-
-	if [ -n "$argv" ];
-		switch "$argv"
-		case "--only"
- 			eval "$curlIP $IPonly"
-		case "--only --copy"
-			eval "$curlIP $IPonly"
-			eval "$curlIP $IPonly | PBCOPY"
-		case "--copy"
-			eval "$curlIP"
-			eval "$curlIP $IPonly | PBCOPY"
-		case "*"
-			eval "$curlIP"
-		end
-
-		return
-	end
-
-	eval "$curlIP"
+# Defined in /var/folders/ht/0ty_c8mx16v69csh949g3rvr0000gn/T//fish.0M7uad/ip.fish @ line 2
+function ip --description 'Get public (external) IP address'
+	curl https://api.ipify.org --silent | pbcopy
+	printf (% 35)(pbpaste)(% 0)\n
 end
