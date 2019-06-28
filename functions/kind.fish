761eac21b815b25d201074e3f6cce688b7cb3570
CK <34463321+ChristoferK@users.noreply.github.com>
2019-06-28 17:23:32 +0100

    ongoing development (partial functionality)

--- functions/kind.fish
+++ functions/kind.fish
@@ -1,9 +1,5 @@
-# Defined in - @ line 2
+# Defined in /var/folders/ht/0ty_c8mx16v69csh949g3rvr0000gn/T//fish.MvMAxj/kind.fish @ line 2
 function kind --description 'Get probable file extensions suitable for the file types detected'
-	function +
-		printf '\e[%sm' "$argv[1]"
-	end
-
 	set -l _NONE 0
 	set -l _FONT_I 3
 	set -l _FONT_U 4
@@ -25,15 +21,34 @@ function kind --description 'Get probable file extensions suitable for the file 
 	set -l _FILL_C 46
 	set -l _FILL_W 47
 
-	set -l exts ( file --brief --extension $argv \
-	            | grep -E -o '^\w+' | string upper )
-	set -l N
+	set mime ( file --brief --mime-type $argv )
+	set N
+
+	set cols ( tput cols )
+	set tablen ( string length \t )
+	set elipsis "..."
 
 	for i in (seq (count $argv))
-		printf "%b%b$exts[$i]＊%b %b%b$argv[$i]%b\n\n" \
-		       (+ $_COLOR_K) (+ $_FILL_W) (+ $_NONE) \
-		       (+ $_FONT_I) (+ $_FONT_U) (+ $_NONE)
-	end
+		set filepath "$argv[$i]"
+		set mimetype "＊$mime[$i]"
 
-	# functions -e +
+		set mimelen ( string length "$mimetype" )
+		set pathlen ( string length "$filepath" )
+		
+		[ (= $pathlen + $mimelen + $tablen) -ge $cols ] \
+			&& set numdots 3 \
+			|| set numdots 0
+		
+		set maxcols (= $cols - $mimelen - $tablen - $numdots)
+		echo $maxcols (= $pathlen % $maxcols + 1)
+		set filepath ( string sub -s (= $pathlen % $maxcols + 1) "$filepath" )
+		set pathlen ( string length "$filepath" )
+		set gapsize (= $cols - $numdots - $pathlen + $tablen - $mimelen)
+
+		printf '%s%s%-*s%s\n' \
+		       (string sub -l $numdots "$elipsis") \
+		       (% $_FONT_I $_FONT_U $_COLOR_W)"$filepath"(% $_NONE) \
+		       "$gapsize" \t \
+		       (% $_COLOR_K $_FILL_W)"$mimetype"(% $_NONE)
+	end
 end
