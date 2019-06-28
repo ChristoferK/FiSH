ea2c5abc99674647fbda76e339006b390054b4c6
CK <34463321+ChristoferK@users.noreply.github.com>
2019-06-28 17:18:42 +0100

    system edit

--- functions/%.fish
+++ functions/%.fish
@@ -1,3 +1,4 @@
-function % --description 'Prints an escape sequence that styles terminal output'
+# Defined in /var/folders/ht/0ty_c8mx16v69csh949g3rvr0000gn/T//fish.6dMA7M/%.fish @ line 2
+function '%' --description 'Prints an escape sequence that styles terminal output'
 	printf '\e[%sm' $argv
 end
