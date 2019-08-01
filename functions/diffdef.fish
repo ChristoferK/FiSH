6c1fd3c75bf2b3f69841e25114552d28e5fac2a4
CK <34463321+ChristoferK@users.noreply.github.com>
2019-08-01 11:28:04 +0100

    minor syntax changes

--- functions/diffdef.fish
+++ functions/diffdef.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/ht/0ty_c8mx16v69csh949g3rvr0000gn/T//fish.eJFRdd/diffdef.fish @ line 2
+# Defined in /var/folders/ht/0ty_c8mx16v69csh949g3rvr0000gn/T//fish.bxCskM/diffdef.fish @ line 2
 function diffdef --description 'Compares the user defaults between two separate runs of this command to look for any changes.'
 	set -l def_base_path /tmp/def.global
 	set -l def_files "$def_base_path".*
@@ -6,17 +6,17 @@ function diffdef --description 'Compares the user defaults between two separate 
 	set -l def_file "$def_base_path".(math "$n + 1")
 	set -l def_files $def_files $def_file
 
-	[ $n -gt 1 ]; \
-		and PRINTF '%s\n' "Lingering temp files. Will purge." \
-		 	          "Please rerun the command."; \
-		and RM $def_files; \
-		and return (false)
+	[ $n -gt 1 ] \
+		&& printf '%s\n' "Lingering temp files. Will purge." \
+		 	         "Please rerun the command." \
+		&& command rm $def_files \
+		&& return (false)
 
-	DEFAULTS read > "$def_file"; and PRINTF '%s\n' "Defaults written to $def_file"
+	defaults read > "$def_file" && printf '%s\n' "Defaults written to $def_file"
 
-	[ $n -eq 1 ]; \
-		and DIFF $def_files; \
-		and RM $def_files > /dev/null
+	[ $n -eq 1 ] \
+		&& diff $def_files \
+		&& command rm $def_files > /dev/null
 
 	return $status
 end
